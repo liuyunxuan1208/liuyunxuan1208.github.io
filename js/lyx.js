cbe03b74407ca9ccabbbe26af0d65424a3bc5d5c
liuyunxuan <2126495632@qq.com>
2025-10-23 12:50:31 +0800

    Site updated: 2025-10-23 12:50:21

--- js/lyx.js
+++ js/lyx.js
@@ -1,1 +1,14 @@
-function show_date_time(){var t=new Date("1/20/2021 0:0:0"),o=(new Date).getTime()-t.getTime(),e=o/1e3,f=(Math.floor(e),o/864e5),n=Math.floor(f),a=24*(f-n),l=Math.floor(a),r=60*(a-l),d=Math.floor(60*(a-l)),i=Math.floor(60*(r-d));$("#runtime").html('<span id="span_dt_dt" style="color: #131212;"><font style="color:#afb4db;">'+n+'</font> 天 <font style="color:#f391a9;">'+l+'</font> 时 <font style="color:#fdb933;">'+d+'</font> 分 <font style="color:#a3cf62;">'+i+"</font> 秒"),window.setTimeout(show_date_time,1e3)}$(document).ready((function(t){show_date_time()}));
+function show_date_time() {
+    var t = new Date("1/20/2021 0:0:0");
+    var o = (new Date()).getTime() - t.getTime();
+    var seconds = Math.floor(o / 1000);
+    var days = Math.floor(seconds / (3600 * 24));
+    var hours = Math.floor((seconds % (3600 * 24)) / 3600);
+    var minutes = Math.floor((seconds % 3600) / 60);
+    var secs = seconds % 60;
+    $("#runtime").html('<span id="span_dt_dt" style="color: #131212;"><font style="color:#afb4db;">' + days + '</font> 天 <font style="color:#f391a9;">' + hours + '</font> 时 <font style="color:#fdb933;">' + minutes + '</font> 分 <font style="color:#a3cf62;">' + secs + "</font> 秒");
+    window.setTimeout(show_date_time, 1000);
+}
+$(document).ready(function() {
+    show_date_time();
+});
